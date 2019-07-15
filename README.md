657a26188c3c894cb7ab133f17dfb3e7088473f7
Coenraad Human <coen.human@gmail.com>
2019-07-15 08:03:46 +0200

    POST working on WeatherStation Unit

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # WeatherStation_Unit
 This is the repository for the micro-controllers that we use to make a Station with.
 
+Technologies
+------------
+* Embedded C++
+
+Hardware
+--------
+* NodeMCU (ESP8266) E-12
+* BME
+
+Libraries to be installed on Arduino IDE
+---------------
+
 Reference Links
 ---------------
 * [ESP-Specific APIs](http://arduino.esp8266.com/Arduino/versions/2.1.0/doc/libraries.html)
+* [Arduino JSON Assistant](https://arduinojson.org/v6/assistant/)
+* [POST Data from ESP8266](https://stackoverflow.com/questions/41371156/esp8266-and-post-request)
+
+
--- StationCode/StationCode.ino
+++ StationCode/StationCode.ino
@@ -1,11 +1,12 @@
-#include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>       
 #include <DNSServer.h>
+#include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>    
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h> 
+#include <ESP8266HTTPClient.h>
 
 #define SEALEVELPRESSURE_HPA (1020)
 #define LIGHTSENSORPIN A0
@@ -26,40 +27,44 @@ void setup() {
 void loop() {
     if(WiFi.status() == WL_CONNECTED) {
       if(!isConnected) {
-        digitalWrite(onBoardLED, LOW);
+        digitalWrite(onBoardLED, HIGH);
         isConnected = true;
       }
-      StaticJsonBuffer<300> JSONbuffer;   //Declaring static JSON buffer
-      JsonObject& JSONencoder = JSONbuffer.createObject(); 
- 
-      JSONencoder["Station"] = String(ESP.getChipId());
-      JSONencoder["Temperature"] = String(bme.readTemperature());
-      JSONencoder["Humidity"] = String(bme.readHumidity());
-      JSONencoder["AirPressure"] = String(bme.readPressure());
-      JSONencoder["Light"] = String(analogRead(LIGHTSENSORPIN));
- 
-      char JSONmessageBuffer[300];
-      JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
-      Serial.println(JSONmessageBuffer);
- 
+
+
+      // create json to send
+      const size_t capacity = JSON_OBJECT_SIZE(8);
+      DynamicJsonDocument doc(capacity);
+      doc["Station"] = String(ESP.getChipId());
+      doc["AirPressure"] = String(bme.readPressure());
+      doc["Humidity"] = String(bme.readHumidity());
+      doc["Light"] = String(analogRead(LIGHTSENSORPIN));
+      doc["Temperature"] = String(bme.readTemperature());
+      serializeJson(doc, Serial);
+      Serial.println("");
+
       HTTPClient http;    //Declare object of class HTTPClient
  
-      http.begin("http://51.143.152.177:5000/api/values");      //Specify request destination
+      http.begin("http://51.145.50.160:5000/api/values");      //Specify request destination
       http.addHeader("Content-Type", "application/json");  //Specify content-type header
+
+      String json;
+      serializeJson(doc, json);
+      int httpCode = http.POST(json);   //Send the request
+      
+      String payload = http.getString();                  //Get the response payload
  
-      int httpCode = http.POST(JSONmessageBuffer);   //Send the request
-      String payload = http.getString();                                        //Get the response payload
- 
-      Serial.println(httpCode);   //Print HTTP return code
+      //Serial.println(httpCode);   //Print HTTP return code
       Serial.println(payload);    //Print request response payload
  
-      http.end();  //Close connection
+      http.end();
+ 
     } else {
       if(isConnected) {
-        digitalWrite(onBoardLED, HIGH); 
+        digitalWrite(onBoardLED, LOW); 
         isConnected = false;
       }
       Serial.println("WIFI disconnected!");
     }
-    delay(5000);
+    delay(30000);
 }
