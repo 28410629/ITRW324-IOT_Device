fa3f7e0ed22522c733bbe5a3514539c94d9bfeaa
Coenraad Human <coen.human@gmail.com>
2019-07-20 13:30:38 +0200

    Adjusted code for enduser use

--- StationCode/StationCode.ino
+++ StationCode/StationCode.ino
@@ -18,21 +18,25 @@ const int onBoardLED = 2;
 void setup() {    
     Serial.begin(115200);
     WiFiManager wifiManager;
-    wifiManager.autoConnect("WeatherStation", "connect");
+    wifiManager.autoConnect("WeatherStation : " + ESP.getChipId(), "connect");
     pinMode(2, OUTPUT); // on-board led
     bme.begin(0x76);  
     pinMode(LIGHTSENSORPIN, INPUT);
+    // Print user register information.
+    Serial.println("This is the sketch for interacting with WeatherStation service.");
+    Serial.println("Please use the following Station ID when registering: ");
+    Serial.print(ESP.getChipId());
 }
 
 void loop() {
     if(WiFi.status() == WL_CONNECTED) {
       if(!isConnected) {
+        // Turn alert off when connected to WIFI
         digitalWrite(onBoardLED, HIGH);
         isConnected = true;
       }
-
-
-      // create json to send
+      
+      // Create JSON
       const size_t capacity = JSON_OBJECT_SIZE(8);
       DynamicJsonDocument doc(capacity);
       doc["StationId"] = String(ESP.getChipId());
@@ -40,32 +44,22 @@ void loop() {
       doc["Humidity"] = String(bme.readHumidity());
       doc["AmbientLight"] = String(analogRead(LIGHTSENSORPIN));
       doc["Temperature"] = String(bme.readTemperature());
-      serializeJsonPretty(doc, Serial);
-      
 
-      HTTPClient http;    //Declare object of class HTTPClient
- 
-      http.begin("http://weatherstationapi.ddns.net:5000/api/post/reading");      //Specify request destination
-      http.addHeader("Content-Type", "application/json");  //Specify content-type header
-
+      // Send JSON to Server
+      HTTPClient http;
+      http.begin("http://weatherstationapi.ddns.net:5000/api/post/reading");
+      http.addHeader("Content-Type", "application/json");
       String json;
       serializeJson(doc, json);
-      int httpCode = http.POST(json);   //Send the request
-      
-      String payload = http.getString();                  //Get the response payload
- 
-      Serial.println();
-      Serial.print("Station ID:");    
-      Serial.println(String(ESP.getChipId()));
- 
+      http.POST(json);   //Send the request
       http.end();
  
     } else {
       if(isConnected) {
+        // Alert user that station is disconnected from WIFI
         digitalWrite(onBoardLED, LOW); 
         isConnected = false;
       }
-      
     }
     delay(30000);
 }
