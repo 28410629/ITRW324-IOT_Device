ea968e1bca8388e7e8f63c7c84f39a959102a5af
Coenraad Human <coen.human@gmail.com>
2019-08-08 14:59:19 +0200

    Deep sleep added for stations, reduces power consumption.

--- StationCode/StationCode.ino
+++ StationCode/StationCode.ino
@@ -13,29 +13,26 @@
 
 Adafruit_BME280 bme;
 bool isConnected = false;
-const int onBoardLED = 2; // WIFI Connection
-const int onBoardLED2 = 16; // Device On
+const int onBoardLED = 2;
 
 void setup() {    
     Serial.begin(115200);
     WiFiManager wifiManager;
     wifiManager.autoConnect("WeatherStation : " + ESP.getChipId(), "connect");
-    pinMode(onBoardLED, OUTPUT);
-    pinMode(onBoardLED2, OUTPUT); 
-    digitalWrite(onBoardLED, LOW);
+    pinMode(onBoardLED, OUTPUT); // on-board led
     bme.begin(0x76);  
     pinMode(LIGHTSENSORPIN, INPUT);
     // Print user register information.
-    Serial.println("This is the sketch for interacting with WeatherStation service.");
-    Serial.println("Please use the following Station ID when registering: ");
-    Serial.print(ESP.getChipId());
+    Serial.println("This is the sketch for interacting with AnchormenStations service.");
+    Serial.print("Please use the following Station ID when registering: ");
+    Serial.println(ESP.getChipId());
 }
 
 void loop() {
     if(WiFi.status() == WL_CONNECTED) {
       if(!isConnected) {
         // Turn alert off when connected to WIFI
-        digitalWrite(onBoardLED, HIGH);
+        digitalWrite(onBoardLED, LOW);
         isConnected = true;
       }
       
@@ -60,9 +57,10 @@ void loop() {
     } else {
       if(isConnected) {
         // Alert user that station is disconnected from WIFI
-        digitalWrite(onBoardLED, LOW); 
+        digitalWrite(onBoardLED, HIGH); 
         isConnected = false;
       }
     }
-    delay(30000);
+    Serial.println("Entering deep sleep mode.");
+    ESP.deepSleep(60000000, WAKE_RF_DEFAULT);
 }
